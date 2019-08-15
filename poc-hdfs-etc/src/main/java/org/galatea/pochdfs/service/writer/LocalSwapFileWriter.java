2425ac0d6c0c2c99b6574d1ac9e25965781af8e7
Rborr <Rborr@rollins.edu>
2019-08-15 12:04:01 -0400

    implemented DataMap to allow creation of less file streams

--- poc-hdfs-etc/src/main/java/org/galatea/pochdfs/service/writer/LocalSwapFileWriter.java
+++ poc-hdfs-etc/src/main/java/org/galatea/pochdfs/service/writer/LocalSwapFileWriter.java
@@ -9,8 +9,11 @@ import java.io.FileWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 
+import java.util.Set;
 import org.galatea.pochdfs.utils.hdfs.IHdfsFilePathGetter;
 import org.galatea.pochdfs.utils.hdfs.JsonMapper;
 import org.mortbay.log.Log;
@@ -27,99 +30,163 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class LocalSwapFileWriter {
 
-	private static final ObjectMapper	MAPPER	= new ObjectMapper();
+  private static final ObjectMapper MAPPER = new ObjectMapper();
+  private final SwapFilePathCreator pathCreator;
+  private final JsonMapper objectMapper;
 
-	private final SwapFilePathCreator	pathCreator;
+  private Map<String, Set<String>> dataMap;
+  private final int BUFFER_SIZE = 1000;
 
-	private final JsonMapper			objectMapper;
+  private long recordsLoggedPerSecondCounter = 0;
+  private long recordsLoggedPerSecondStartTime;
 
-	@SneakyThrows
-	public void writeSwapData(final String localFilePath, final String targetBasePath) {
-		File file = new File(localFilePath);
-		String filename = file.getName();
-		log.info("Writing {} data", filename);
+  @SneakyThrows
+  public void writeSwapData(final String localFilePath, final String targetBasePath) {
+    File file = new File(localFilePath);
+    String filename = file.getName();
 
-		if (filename.toLowerCase().contains("counterparties")) {
-			writeStringToFile(new String(Files.readAllBytes(file.toPath())), pathCreator.createCounterpartyFilepath(),
-					targetBasePath);
-		} else if (filename.toLowerCase().contains("instruments")) {
-			writeStringToFile(new String(Files.readAllBytes(file.toPath())), pathCreator.createInstrumentsFilepath(),
-					targetBasePath);
-		} else if (filename.toLowerCase().contains("swapcontracts")) {
-			writeRecords(file, targetBasePath, (jsonObject) -> {
-				return pathCreator.constructSwapContractFilepath((int) jsonObject.get("counterparty_id"));
-			});
-		} else if (filename.toLowerCase().contains("positions")) {
-			writeRecords(file, targetBasePath, (jsonObject) -> {
-				return pathCreator.createPositionFilepath((int) jsonObject.get("swap_contract_id"));
-			});
-		} else if (filename.toLowerCase().contains("cashflows")) {
-			writeRecords(file, targetBasePath, (jsonObject) -> {
-				return pathCreator.createCashFlowFilepath((int) jsonObject.get("swap_contract_id"));
-			});
-		} else {
-			throw new FileNotFoundException("File " + filename + "not found");
-		}
-	}
+    log.info("Writing {} data", filename);
 
-	@SneakyThrows
-	private void writeRecords(final File file, final String targetBasePath, final IHdfsFilePathGetter pathGetter) {
+    if (filename.toLowerCase().contains("counterparties")) {
+      writeStringToFile(new String(Files.readAllBytes(file.toPath())),
+          pathCreator.createCounterpartyFilepath(),
+          targetBasePath);
+    } else if (filename.toLowerCase().contains("instruments")) {
+      writeStringToFile(new String(Files.readAllBytes(file.toPath())),
+          pathCreator.createInstrumentsFilepath(),
+          targetBasePath);
+    } else if (filename.toLowerCase().contains("swapcontracts")) {
+      writeRecords(file, targetBasePath, (jsonObject) -> {
+        return pathCreator.constructSwapContractFilepath((int) jsonObject.get("counterparty_id"));
+      });
+    } else if (filename.toLowerCase().contains("positions")) {
+      writeRecords(file, targetBasePath, (jsonObject) -> {
+        return pathCreator.createPositionFilepath((int) jsonObject.get("swap_contract_id"));
+      });
+    } else if (filename.toLowerCase().contains("cashflows")) {
+      writeRecords(file, targetBasePath, (jsonObject) -> {
+        return pathCreator.createCashFlowFilepath((int) jsonObject.get("swap_contract_id"));
+      });
+    } else {
+      throw new FileNotFoundException("File " + filename + "not found");
+    }
+  }
 
-		try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+  @SneakyThrows
+  private void writeRecords(final File file, final String targetBasePath,
+      final IHdfsFilePathGetter pathGetter) {
 
-			long recordsLoggedPerSecondStartTime = System.currentTimeMillis();
-			long recordsLoggedPerSecondCounter = 0;
+    try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
 
-			int recordCount = 0;
-			String jsonLine = reader.readLine();
+      dataMap = new HashMap<String, Set<String>>();
 
-			while (jsonLine != null) {
-				Long startTime = System.currentTimeMillis();
-				Long individualProcessStartTime = System.currentTimeMillis();
+      recordsLoggedPerSecondStartTime = System.currentTimeMillis();
+      Long startTime = System.currentTimeMillis();
+      long totalSeconds = 0;
+      long totalRecordsProcessed = 0;
 
-				Map<String, Object> jsonObject = objectMapper.getTimestampedObject(jsonLine);
+      int recordCount = 0;
+      String jsonLine = reader.readLine();
 
-				log.info("Finished object mapping in {} ms", System.currentTimeMillis() - individualProcessStartTime);
-				individualProcessStartTime = System.currentTimeMillis();
+      while (jsonLine != null) {
 
-				String filePath = pathGetter.getFilePath(jsonObject);
+        Long individualProcessStartTime = System.currentTimeMillis();
 
-				log.info("Retrieved FilePath in {} ms", System.currentTimeMillis()-individualProcessStartTime);
-				individualProcessStartTime = System.currentTimeMillis();
+        Map<String, Object> jsonObject = objectMapper.getTimestampedObject(jsonLine);
 
-				StringBuilder builder = new StringBuilder(MAPPER.writeValueAsString(jsonObject));
-				String recordData = builder.append("\n").toString();
+        log.debug("Finished object mapping in {} ms",
+            System.currentTimeMillis() - individualProcessStartTime);
+        individualProcessStartTime = System.currentTimeMillis();
 
-				writeStringToFile(recordData, filePath, targetBasePath);
+        String filePath = pathGetter.getFilePath(jsonObject);
 
-				log.info("Wrote object {} to file", recordCount);
-				log.info("Wrote object to file in {} ms", System.currentTimeMillis()-individualProcessStartTime);
-				log.info("Total time to write one object file {} ms", System.currentTimeMillis() - startTime);
+        log.debug("Retrieved FilePath in {} ms",
+            System.currentTimeMillis() - individualProcessStartTime);
+        individualProcessStartTime = System.currentTimeMillis();
 
-				recordCount++;
-				recordsLoggedPerSecondCounter++;
-				jsonLine = reader.readLine();
+        StringBuilder builder = new StringBuilder(MAPPER.writeValueAsString(jsonObject));
+        String recordData = builder.append("\n").toString();
 
-				if(System.currentTimeMillis() - recordsLoggedPerSecondStartTime > 1000){
-					log.info("******** Records logged in last second: {} *********", recordsLoggedPerSecondCounter);
-					recordsLoggedPerSecondCounter = 0;
-					recordsLoggedPerSecondStartTime = System.currentTimeMillis();
-				}
+        //writeStringToFile(recordData, filePath, targetBasePath);
+        addDataToDataMap(targetBasePath + filePath, recordData);
 
-			}
-		}
-	}
+        log.debug("Added object {} to Map", recordCount);
+        log.debug("Wrote object to file in {} ms",
+            System.currentTimeMillis() - individualProcessStartTime);
+        log.debug("Total time to write one object file {} ms",
+            System.currentTimeMillis() - startTime);
+        recordCount++;
 
-	@SneakyThrows
-	private void writeStringToFile(final String data, final String filePath, final String targetBasePath) {
-		log.info(targetBasePath);
-		log.info(filePath);
-		Files.createDirectories(Paths.get(targetBasePath + filePath).getParent());
-		try (BufferedWriter writer = new BufferedWriter(new FileWriter(targetBasePath + filePath, true))) {
-			writer.write(data);
+        jsonLine = reader.readLine();
 
-		}
-	}
+        if (System.currentTimeMillis() - recordsLoggedPerSecondStartTime > 1000) {
+          log.info("******** Records logged in last second: {} *********",
+              recordsLoggedPerSecondCounter);
+
+          totalRecordsProcessed += recordsLoggedPerSecondCounter;
+          totalSeconds++;
+          log.info("******** Average Records logged per second {} ********",
+              totalRecordsProcessed / totalSeconds);
+
+          recordsLoggedPerSecondCounter = 0;
+          recordsLoggedPerSecondStartTime = System.currentTimeMillis();
+        }
+      }
+      clearMap();
+      log.info("Process Completed in {} ms", System.currentTimeMillis() - startTime);
+    }
+  }
+
+  private void addDataToDataMap(String filePath, String data) {
+    if (dataMap.containsKey(filePath)) {
+      dataMap.get(filePath).add(data);
+    } else {
+      HashSet<String> dataSet = new HashSet<String>();
+      dataSet.add(data);
+      dataMap.put(filePath, dataSet);
+    }
+    checkMapSize(filePath);
+  }
+
+  private void clearMap() {
+    for (String filePath : dataMap.keySet()) {
+      writeDataSetToFile(filePath);
+    }
+    dataMap.clear();
+  }
+
+  private void checkMapSize(String filePath) {
+    if (dataMap.get(filePath).size() >= BUFFER_SIZE) {
+      writeDataSetToFile(filePath);
+    }
+  }
+
+
+  @SneakyThrows
+  private void writeDataSetToFile(String filePath) {
+    log.info("Writing Data to File {} ", filePath);
+    Files.createDirectories(Paths.get(filePath).getParent());
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true))) {
+      Set<String> dataSet = dataMap.get(filePath);
+      for (String data : dataSet) {
+        writer.write(data);
+        recordsLoggedPerSecondCounter++;
+      }
+      dataMap.put(filePath, new HashSet<String>());
+    }
+  }
+
+  @SneakyThrows
+  private void writeStringToFile(final String data, final String filePath,
+      final String targetBasePath) {
+    log.info(targetBasePath);
+    log.info(filePath);
+    Files.createDirectories(Paths.get(targetBasePath + filePath).getParent());
+    try (BufferedWriter writer = new BufferedWriter(
+        new FileWriter(targetBasePath + filePath, true))) {
+      writer.write(data);
+    }
+  }
 
 
 }
